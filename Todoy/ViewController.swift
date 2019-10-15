1008f26ee8ca969edd39a47957e48039683dda68
Njål <Njaltk@gmail.com>
2019-10-15 09:12:35 +0200

    Initial Commit

---
+++ Todoy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoy
+//
+//  Created by Njål Torgnes Kristensen on 15/10/2019.
+//  Copyright © 2019 Norsk Elæring. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
